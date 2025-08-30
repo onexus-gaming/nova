a0e137fe3ed1b924e905547fc981a54da91050c3
Mizu <40831463+Rexxt@users.noreply.github.com>
2025-08-30 21:51:44 +0200

    fancy images

--- default_initial.lua
+++ default_initial.lua
@@ -1,7 +1,12 @@
 local InitialScene = nova.Scene:extend()
 
+local nova_logo = love.graphics.newImage("lib/nova/img/lw.png")
+local w, h = nova_logo:getDimensions()
+
 function InitialScene:draw()
+    local vw, vh = love.graphics.getDimensions()
     love.graphics.print("nova: no initial scene was found.\ncreate a scene inside of scenes/initial.lua.\n\nrunning nova v" .. nova.version .. "\nonexus, ".. nova.year)
+    love.graphics.draw(nova_logo, vw - w, vh - h)
 end
 
 return InitialScene

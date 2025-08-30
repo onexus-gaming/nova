3ae7ef03e2d5e54a83757e64c196f7e5430bc414
Mizu <40831463+Rexxt@users.noreply.github.com>
2025-08-30 21:54:03 +0200

    tiny visual change

--- default_initial.lua
+++ default_initial.lua
@@ -5,8 +5,9 @@ local w, h = nova_logo:getDimensions()
 
 function InitialScene:draw()
     local vw, vh = love.graphics.getDimensions()
+    local pad = math.floor(vw/50)
     love.graphics.print("nova: no initial scene was found.\ncreate a scene inside of scenes/initial.lua.\n\nrunning nova v" .. nova.version .. "\nonexus, ".. nova.year)
-    love.graphics.draw(nova_logo, vw - w, vh - h)
+    love.graphics.draw(nova_logo, vw - w - pad, vh - h - pad)
 end
 
 return InitialScene

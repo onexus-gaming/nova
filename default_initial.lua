7a4116ab0ee638d95b4f8bb1b8ef7cbcbd38b4a7
Mizu <40831463+Rexxt@users.noreply.github.com>
2025-08-30 22:14:19 +0200

    ui scaling in the default initial scene

--- default_initial.lua
+++ default_initial.lua
@@ -5,9 +5,10 @@ local w, h = nova_logo:getDimensions()
 
 function InitialScene:draw()
     local vw, vh = love.graphics.getDimensions()
-    local pad = math.floor(vw/50)
-    love.graphics.print("nova: no initial scene was found.\ncreate a scene inside of scenes/initial.lua.\n\nrunning nova v" .. nova.version .. "\nonexus, ".. nova.year)
-    love.graphics.draw(nova_logo, vw - w - pad, vh - h - pad)
+    local scale = nova.UI:getScalingCoefficient()
+    local pad = math.floor(16 * scale)
+    love.graphics.print("nova: no initial scene was found.\ncreate a scene inside of scenes/initial.lua.\n\nrunning nova v" .. nova.version .. "\nonexus, ".. nova.year .. "\n\nscale: " .. scale .. "x")
+    love.graphics.draw(nova_logo, math.floor(vw - w * scale - pad), math.floor(vh - h * scale - pad), 0, scale)
 end
 
 return InitialScene

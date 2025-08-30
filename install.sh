2866b31d61bd42c698d64e197fba4e8f5d76e701
Mizu <40831463+Rexxt@users.noreply.github.com>
2025-08-30 20:56:46 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo "nova installer v$VER"
 echo "onexus, $YEAR"
 
 echo "use git for version control? (repository must already be set up)"
-read -p "[y/N]" use_git
+read -p "[y/N] " use_git
 case "$use_git" in
     "y"|"Y")
         git submodule add https://github.com/onexus-gaming/nova lib/nova

061970a213d6c3baa49bacc23e5d94bb08c29559
Mizu <40831463+Rexxt@users.noreply.github.com>
2025-08-30 21:29:44 +0200

    more initial work

---
+++ classes/Scene.lua
@@ -0,0 +1,7 @@
+local Scene = Object:extend()
+
+function Scene:opened() end
+function Scene:update(dt) end
+function Scene:draw() end
+
+return Scene
--- init.lua
+++ init.lua
@@ -5,6 +5,44 @@
 binser = require "lib.binser"
 Object = require "lib.classic"
 
-local nova = {}
+local Scene = require "lib.nova.classes.Scene"
+
+local nova = {
+    scenes = {
+        loaded = {},
+        current = Scene(),
+        load = function(self, name)
+            self.loaded[name] = require("scenes." .. name)
+        end,
+    },
+
+    services = {},
+
+    Scene = Scene,
+}
+
+-- loading scenes
+if not pcall(function() require "scenes.initial" end) then
+    nova.scenes.loaded.initial = require "lib.nova.default_initial"
+end
+
+local hook_table = {"load", "update", "draw"}
+for i, v in ipairs(hook_table) do
+    local original_function = love[v]
+
+    love[v] = function(...)
+        for j, svc in ipairs(nova.services) do
+            if svc[v] then
+                svc[v](...)
+            end
+        end
+        if nova.scenes.current[v] then
+            nova.scenes.current[v](...)
+        end
+        original_function(...)
+    end
+end
+
+nova.scenes.current:opened()
 
 return nova

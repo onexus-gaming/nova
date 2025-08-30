cf7139547a9c51ddb2f813cf8ab83f5de3040581
Mizu <40831463+Rexxt@users.noreply.github.com>
2025-08-30 22:20:25 +0200

    beter initial screen

--- default_initial.lua
+++ default_initial.lua
@@ -4,11 +4,13 @@ local nova_logo = love.graphics.newImage("lib/nova/img/lw.png")
 local w, h = nova_logo:getDimensions()
 
 function InitialScene:draw()
+    love.graphics.clear(0.4,0.4,0.4)
     local vw, vh = love.graphics.getDimensions()
     local scale = nova.UI:getScalingCoefficient()
-    local pad = math.floor(16 * scale)
+    love.graphics.setColor(1,1,1,0.2)
+    love.graphics.draw(nova_logo, math.floor(vw/2 - w * scale/2), math.floor(vh/2 - h * scale/2), 0, scale)
+    love.graphics.setColor(1,1,1,1)
     love.graphics.print("nova: no initial scene was found.\ncreate a scene inside of scenes/initial.lua.\n\nrunning nova v" .. nova.version .. "\nonexus, ".. nova.year .. "\n\nscale: " .. scale .. "x")
-    love.graphics.draw(nova_logo, math.floor(vw - w * scale - pad), math.floor(vh - h * scale - pad), 0, scale)
 end
 
 return InitialScene
